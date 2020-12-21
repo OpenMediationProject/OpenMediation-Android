8e02d30008afe6964447087e949a44c4988107e6
messi <zwmessi@163.com>
2020-12-21 20:10:54 +0800

    Delete

--- om-network/src/main/java/com/openmediation/sdk/adn/core/BaseAdListener.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2019 ADTIMING TECHNOLOGY COMPANY LIMITED
-// Licensed under the GNU Lesser General Public License Version 3
-
-package com.openmediation.sdk.adn.core;
-
-public interface BaseAdListener {
-
-}
--- om-network/src/main/java/com/openmediation/sdk/adn/utils/error/ErrorBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.openmediation.sdk.adn.utils.error;
-
-public class ErrorBuilder {
-
-    public static Error build(int code) {
-        return new Error(code, ErrorCode.getErrorMessage(code));
-    }
-}
